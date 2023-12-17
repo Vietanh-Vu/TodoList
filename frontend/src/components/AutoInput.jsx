7b3fb8700349d202009cd4a545f72416038be61f
Vietanh-Vu <vuvietanh993@gmail.com>
2023-12-17 14:43:08 +0700

    fix: BASE_URL

--- frontend/src/components/AutoInput.jsx
+++ frontend/src/components/AutoInput.jsx
@@ -4,7 +4,7 @@ import TextField from "@mui/material/TextField";
 import axios from "axios";
 import { AutocompleteOption, ListItemContent } from "@mui/joy";
 
-const BASE_URL = "https://fa51-116-96-45-139.ngrok-free.app/api/trie";
+const BASE_URL = "https://dc57-116-96-45-139.ngrok-free.app/api/trie";
 // const BASE_URL = "http://localhost:8080/api/trie";
 
 export default function AutoInput(props) {
